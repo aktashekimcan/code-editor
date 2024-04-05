5ca027c0a46c37161f77d781bb931e70573e6671
HEKİMCAN AKTAŞ <dawissmark@gmail.com>
2024-04-05 21:40:12 +0300

    third initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { CodeProvider } from "../context/CodeContext"; // Make sure this path is correct
