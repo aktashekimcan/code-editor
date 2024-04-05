e216c89531255baed1f148fa2501f72e4d9a3f71
aktashekim <108878518+aktashekimcan@users.noreply.github.com>
2024-04-05 21:19:08 +0300

    Updated CodeContext.tsx

--- context/CodeContext.tsx
+++ context/CodeContext.tsx
@@ -1,17 +1,26 @@
-// CodeContext.js
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from "react";
 
-const CodeContext = createContext();
+// Provide a default context value that matches the shape of your context
+const CodeContext = createContext({
+  htmlCode: "",
+  setHtmlCode: () => {},
+  cssCode: "",
+  setCssCode: () => {},
+  jsCode: "",
+  setJsCode: () => {},
+});
 
 export const useCode = () => useContext(CodeContext);
 
 export const CodeProvider = ({ children }) => {
-  const [htmlCode, setHtmlCode] = useState('');
-  const [cssCode, setCssCode] = useState('');
-  const [jsCode, setJsCode] = useState('');
+  const [htmlCode, setHtmlCode] = useState("");
+  const [cssCode, setCssCode] = useState("");
+  const [jsCode, setJsCode] = useState("");
 
   return (
-    <CodeContext.Provider value={{ htmlCode, setHtmlCode, cssCode, setCssCode, jsCode, setJsCode }}>
+    <CodeContext.Provider
+      value={{ htmlCode, setHtmlCode, cssCode, setCssCode, jsCode, setJsCode }}
+    >
       {children}
     </CodeContext.Provider>
   );
